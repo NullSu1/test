1f1881004e4ee6c4b09759503946a5354668145d
suxiaoqiang <xqiangSu@gmail.com>
2021-02-01 14:23:07 +0800

    'update'

--- likou.php
+++ likou.php
@@ -77,29 +77,72 @@ function decrypt($data, $key)
 	return $str;
 }
 
-$key = getCode(18);
-//var_dump($str = encrypt(base64_encode("['status' => 500, 'data' => '', 'massage' => 'Request data error', 'massage' => 'Request data error']"), $key));
-////var_dump(decrypt(encrypt("['status' => 500, 'data' => '', 'massage' => 'Request data error']", '$key'), '$key'));
-//var_dump(str_pad($str,128));
-//var_dump(substr($str,'0','128'));
-//die();
+/**
+ * 解密数据
+ * @param $string
+ * @param string $key
+ * @param bool $operation
+ * @param int $expiry
+ * @return string
+ */
+function authcode($string, $key = '', $operation = false, $expiry = 0)
+{
+	$ckey_length = 4;
+	$key = md5($key ? $key : "DEFAULT_KEYS");
+	$keya = md5(substr($key, 0, 16));
+	$keyb = md5(substr($key, 16, 16));
+	$keyc = $ckey_length ? ($operation ? substr($string, 0, $ckey_length) : substr(md5(microtime()), -$ckey_length)) : '';
 
-
-
-
-
-$fileName = "backiee.jpg";
-$file = "http://192.168.6.75/test/bootstrap/img/" . $fileName;
-
-
-
-//$fileContents = file_get_contents($file);
-if (!$fileContents = file_get_contents($file)) {
-	echo "<script>alert('打开文件失败')</script>";
+	$cryptkey = $keya . md5($keya . $keyc);
+	$key_length = strlen($cryptkey);
+	$string = $operation ? base64_decode(substr($string, $ckey_length)) :
+		sprintf('%010d', $expiry ? $expiry + time() : 0) . substr(md5($string . $keyb), 0, 16) . $string;
+	$string_length = strlen($string);
+	$result = '';
+	$box = range(0, 255);
+	$rndkey = array();
+	for ($i = 0; $i <= 255; $i++) {
+		$rndkey[$i] = ord($cryptkey[$i % $key_length]);
+	}
+	for ($j = $i = 0; $i < 256; $i++) {
+		$j = ($j + $box[$i] + $rndkey[$i]) % 256;
+		$tmp = $box[$i];
+		$box[$i] = $box[$j];
+		$box[$j] = $tmp;
+	}
+	for ($a = $j = $i = 0; $i < $string_length; $i++) {
+		$a = ($a + 1) % 256;
+		$j = ($j + $box[$a]) % 256;
+		$tmp = $box[$a];
+		$box[$a] = $box[$j];
+		$box[$j] = $tmp;
+		$result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
+	}
+	if ($operation) {
+		if ((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) &&
+			substr($result, 10, 16) == substr(md5(substr($result, 26) . $keyb), 0, 16)) {
+			return substr($result, 26);
+		} else {
+			return '';
+		}
+	} else {
+		return $keyc . str_replace('=', '', base64_encode($result));
+	}
 }
-header('Content-Type:text/html;charset=utf-8');
-header('Content-Length: ' . (strlen($fileContents)));
-header('Content-Disposition: attachment; filename=' . $fileName);
 
-exit($fileContents);
+var_dump(authcode('qweq1231','key2w3o9u'));
+var_dump(authcode('f97aFJPslDPYwUitj9PocdJlbgNM3HwEVF0fFIHH6BUrYZUDCg','key2w3o9u',true));
+
+//$fileName = "backiee.jpg";
+//$file = "http://192.168.6.75/test/bootstrap/img/" . $fileName;
+//
+////$fileContents = file_get_contents($file);
+//if (!$fileContents = file_get_contents($file)) {
+//	echo "<script>alert('打开文件失败')</script>";
+//}
+//header('Content-Type:text/html;charset=utf-8');
+//header('Content-Length: ' . (strlen($fileContents)));
+//header('Content-Disposition: attachment; filename=' . $fileName);
+//
+//exit($fileContents);
 
